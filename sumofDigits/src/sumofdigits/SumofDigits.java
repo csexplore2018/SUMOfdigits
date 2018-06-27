07d16030883c805e1485e3d0bb517c3e362ccaa2
gc_science6 <gc_science6@SE135-04.ad.svsu.edu>
2018-06-27 09:54:28 -0400

    sum of digits 1.0.0

---
+++ sumofDigits/src/sumofdigits/SumofDigits.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sumofdigits;
+
+/**
+ *
+ * @author gc_science6
+ */
+public class SumofDigits {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("The SUMOfdigits is not a SUMO WRESTLER!");
+    }
+    
+}
